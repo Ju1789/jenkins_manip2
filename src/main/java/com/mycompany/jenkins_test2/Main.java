9f0e03a3879daa89e9a907c17cdc9763d7901ea3
Julien V <j.veret@hotmail.fr>
2021-10-05 23:39:20 -0400

    first

---
+++ src/main/java/com/mycompany/jenkins_test2/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jenkins_test2;
+
+/**
+ *
+ * @author jveret
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bonjour " + args[1]+ "," + args[0]);
+    }
+}
